5049e1e92425ca89ae26f408599f0bc4c1d2c8cd
miamisudbury <welcome@miamisudburyschool.org>
2025-04-28 09:46:09 -0400

    use new search system for debug
    surely this won't come back to haunt me!

--- debug.py
+++ debug.py
@@ -4,7 +4,6 @@ from time import localtime, strftime
 import requests
 from datetime import datetime
 import json
-import re
 
 def calcTimeSignIn():
     rawtime = localtime()
@@ -49,6 +48,39 @@ def dumpInfo():
     file.close
     print("Dumped response data to response.json")
 
+def evalUser():
+    global personid
+    global name
+    global people
+    global searchfor # are these last 4 necessary? probably not
+    global found
+    global file
+    global out
+    personid = input('Enter PersonID, Name, or PIN of user to check: ')
+    try:
+        file = open("response.json", "r")
+    except FileNotFoundError:
+        print("Could not find response file! Attempting to generate response file...")
+        dumpInfo()
+        file = open("response.json", "r")
+    people = json.load(file)
+    searchfor = personid
+    found = False
+    for i in people:
+        if (i["name"]) == searchfor or (i["pin"]) == searchfor or (str(i["personId"])) == searchfor:
+            if (i["name"]) == "Admin": # no clue if this check is even necessary
+                print("You cannot view the data of this user!")
+                time.sleep(5)
+                exit(100)
+            personid = int(i["personId"])
+            name = i["name"]
+            found = True
+            out = json.dumps(i, indent=2)
+    if found != True:
+        print("The specified person could not be found. Did you type the info correctly? (See documentation: Troubleshooting for more info.)")
+        time.sleep(5)
+        exit(100)
+    return personid
 
 os.system('cls')
 print('''
@@ -82,29 +114,11 @@ elif selectedOption == '1':
 
 elif selectedOption == '2':
     dumpInfo()
-    file = open("response.json", "r")
-    people = json.load(file)
-    
-    searchfor = input("Enter Person ID or Name: ")
-    if re.search("^[0-9]{5}$", searchfor) == None:
-        for i in people:
-            if (i["name"]) == searchfor:
-                found = i
-                out = json.dumps(i, indent=2)
-        try:
-            print(out)
-        except:
-            print("The user you are trying to look for could not be found. Did you spell their name correctly?")
-                
-    if re.search("^[0-9]{5}$", searchfor) != None:
-        for i in people:
-            if (i["personId"]) == int(searchfor):
-                found = i
-                out = json.dumps(i, indent=2)
-        try:
-            print(out)
-        except:
-            print("The user you are trying to look for could not be found. Did you type their ID correctly?")
+    evalUser()
+    try:
+        print(out)
+    except:
+        print("The user you are trying to look for could not be found. Did you type their ID correctly?")
 
     time.sleep(10)
     # Returns raw (formatted) JSON. 
@@ -119,7 +133,7 @@ elif selectedOption == '3':
     for i in people:
         personIndex += 1
         personList.append(personIndex)
-        if (i["name"]) == "Admin":
+        if (i["name"]) == "Admin": # redundant check?
             continue
         if (i["current_day_start_time"]) == None:
             print(str(personIndex) + " - " + i["name"] + " - " + str(i["personId"]))
@@ -164,7 +178,7 @@ elif selectedOption == '4':
     for i in people:
         personIndex += 1
         personList.append(personIndex)
-        if (i["name"]) == "Admin":
+        if (i["name"]) == "Admin": # redundant check?
             continue
         if (i["current_day_end_time"]) == None:
             print(str(personIndex) + " - " + i["name"] + " - " + str(i["personId"]))
